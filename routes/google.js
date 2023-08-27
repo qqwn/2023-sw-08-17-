41b0a157757023966028ae002b26e63e873003b4
qqwn <99554755+qqwn@users.noreply.github.com>
2023-08-28 02:37:33 +0900

    modify port number

--- routes/google.js
+++ routes/google.js
@@ -13,7 +13,7 @@ passport.use(
         {
             clientID: GOOGLE_CLIENT_ID,
             clientSecret: GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:8080/auth/google/callback",
+            callbackURL: "http://localhost:3000/auth/google/callback",
             passReqToCallback: true,
         },
         function (request, accessToken, refreshToken, profile, done) {
